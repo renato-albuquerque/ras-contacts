8fc9a30c20cfeb45a153463d7af8876b48eaba75
renato-albuquerque <renatomalb83@gmail.com>
2023-02-23 21:56:36 -0300

    updated info function share - js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ buttonDarkMode.addEventListener('click', function() {
 function share(){
 	if (navigator.share !== undefined) {
 		navigator.share({
-			title: 'Digital Business Card - Renato Albuquerque',
-			text: 'Front-End Web Developer',
-			url: 'https://rma-contacts.vercel.app/'
+			title: 'Cartão de Visita Digital - Rivadal Silva',
+			text: 'Advogado Tributarista',
+			url: 'https://cartaodevisita-rivadal.vercel.app/'
 		})
 		.then(() => console.log('Successful share'))
 		.catch((error) => console.log('Error sharing', error));
